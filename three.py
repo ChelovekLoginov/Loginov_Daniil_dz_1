577a48afca59ad1f115e9ca571c61295f735ce48
Daniil <892971@gmail.com>
2021-08-17 07:33:39 +0200

    Task_1_1

--- three.py
+++ three.py
@@ -1,14 +1,14 @@
 text1 = " процент"
 text2 = " процента"
 text3 = " процентов"
-cou = 0
-for cou in range(0, 100):
-    cou += 1
+for cou in range(0, 101):
     if 5 <= cou <= 20:
         print(str(cou) + text3)
     elif cou % 10 == 1:
         print(str(cou) + text1)
-    elif cou % 10 == (5, 9):
+    elif 5 <= cou % 10 <= 9:
+        print(str(cou) + text3)
+    elif cou % 10 == 0:
         print(str(cou) + text3)
     else:
         print(str(cou) + text2)
